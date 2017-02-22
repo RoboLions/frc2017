a4c236a57392037e28150d7107313993b3498096
Sumedh <sumedhgarimella@gmail.com>
2017-02-21 20:48:45 -0500

    updated port numbers

--- src/org/usfirst/frc/team1261/robot/RobotMap.java
+++ src/org/usfirst/frc/team1261/robot/RobotMap.java
@@ -12,15 +12,15 @@ import edu.wpi.first.wpilibj.Encoder;
  * floating around.
  */
 public class RobotMap {
-	public static CANTalon leftDriveMotorFront = new CANTalon(5);
-	public static CANTalon leftDriveMotorRear = new CANTalon(7);
-	public static CANTalon rightDriveMotorFront = new CANTalon(6);
-	public static CANTalon rightDriveMotorRear = new CANTalon(8);
-	public static Encoder leftDriveEncoder = new Encoder(5, 7);
-	public static Encoder rightDriveEncoder = new Encoder(6, 8);
-	public static CANTalon intakeMotor = new CANTalon(9);
-	public static CANTalon hopperAgitatorMotor = new CANTalon(3);
-	public static CANTalon climbMotor = new CANTalon(4);
+	public static CANTalon leftDriveMotorFront = new CANTalon(15);
+	public static CANTalon leftDriveMotorRear = new CANTalon(14);
+	public static CANTalon rightDriveMotorFront = new CANTalon(0);
+	public static CANTalon rightDriveMotorRear = new CANTalon(1);
+	public static Encoder leftDriveEncoder = new Encoder(15, 14);
+	public static Encoder rightDriveEncoder = new Encoder(0, 1);
+	public static CANTalon intakeMotor = new CANTalon(4);
+	public static CANTalon hopperAgitatorMotor = new CANTalon(5);
+	public static CANTalon climbMotor = new CANTalon(13);
 	public static CANTalon topFlywheelMotor = new CANTalon(39);
 	public static CANTalon bottomFlywheelMotor = new CANTalon(40);
 	public static CANTalon turretRotationMotor = new CANTalon(41);
